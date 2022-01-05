9530950b3f09967b24d76150edd7fe8215070212
briannaso <sosa.brianna8@gmail.com>
2022-01-05 00:05:56 -0500

    adding cities.js

--- Simple_Map/Mapping_Multiple_Points/js/logic.js
+++ Simple_Map/Mapping_Multiple_Points/js/logic.js
@@ -59,5 +59,4 @@ let cities = [{
 // Loop through the cities array and create one marker for each city.
 cities.forEach(function(city) {
     console.log(city)
-    L.marker(city.location).addTo(map);
-});
+   });
